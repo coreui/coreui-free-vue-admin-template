45b66fe416e401cf3f02f7a6276e76259a11b96c
Rubén Salvador García San Juan <rssanjuan2704@gmail.com>
2017-11-02 12:07:52 -0400

    [FIX] Unit test coverage.

--- Vue_Starter/test/unit/specs/Hello.spec.js
+++ Vue_Starter/test/unit/specs/Hello.spec.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import Hello from '@/components/Hello'
+import Dashboard from '@/views/Dashboard'
 
-describe('Hello.vue', () => {
+describe('Dashboard.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello)
+    const Constructor = Vue.extend(Dashboard)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App')
+    expect(vm.$el.querySelector('.text-info').textContent)
+      .to.equal('Hello World')
   })
 })
