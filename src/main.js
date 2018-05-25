0e1582650ecf2d1ded60f96c0c66f30da55d9f38
xidedix <xidedix@users.noreply.github.com>
2018-05-25 17:54:31 +0200

    refactor: IE polyfills

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'core-js/es6/promise'
+import 'core-js/es6/string'
+import 'core-js/es7/array'
+// import cssVars from 'css-vars-ponyfill'
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
+
+// todo
+// cssVars()
 
 Vue.use(BootstrapVue)
 
