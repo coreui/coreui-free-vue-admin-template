dac5b7445469f48e81fd86cf64cdea2e3f48ef2f
xidedix <xidedix@users.noreply.github.com>
2018-08-21 15:18:53 +0200

    tests: cleanup

--- tests/unit/DefaultAside.spec.js
+++ tests/unit/DefaultAside.spec.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import { shallowMount, mount } from '@vue/test-utils'
 import BootstrapVue from 'bootstrap-vue'
 import DefaultAside from '@/containers/DefaultAside'
 
--- tests/unit/ProgressBars.spec.js
+++ tests/unit/ProgressBars.spec.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { shallowMount, mount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import BootstrapVue from 'bootstrap-vue'
 import ProgressBars from '@/views/base/ProgressBars'
 
