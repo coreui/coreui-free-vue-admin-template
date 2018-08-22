2584eb67eea4ec54ce248baeeb05dff3e3eb1ca7
xidedix <xidedix@users.noreply.github.com>
2018-08-22 14:23:21 +0200

    refactor: testing

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,11 +1,31 @@
-import Vue from 'vue'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
 import App from '@/App'
 
-Vue.use(BootstrapVue)
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+const router = new VueRouter()
+
+
+localVue.use(BootstrapVue)
 
 describe('App.vue', () => {
   it('has a name', () => {
     expect(App.name).toMatch('app')
+  })
+  it('is Vue instance', () => {
+    const wrapper = shallowMount(App, {
+      localVue,
+      router
+    })
+    expect(wrapper.isVueInstance()).toBe(true)
+  })
+  it('is App', () => {
+    const wrapper = shallowMount(App, {
+      localVue,
+      router
+    })
+    expect(wrapper.is(App)).toBe(true)
   })
 })
