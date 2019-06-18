32dec72e0ff62dcbc03f61dca17088ce29b88026
xidedix <xidedix@users.noreply.github.com>
2019-06-18 18:55:29 +0200

    chore: add ie polyfills

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
-    '@vue/app'
+    ['@vue/app', {
+      polyfills: [
+        'es6.array.find',
+        'es6.array.from',
+        'es6.symbol'
+      ]
+    }],
+    ["@babel/preset-env", {
+      "useBuiltIns": "entry"
+    }]
   ]
 }
