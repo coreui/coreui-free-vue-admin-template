dbd97a46fef9a05bc374a4b288a3f2cf699670fb
Maximiliano Sorribas <mjsorribas@gmail.com>
2019-09-26 16:29:09 -0300

    update LogoNavBarItem component to use a json to define brand logo img. Add logoNavBarItems.json to config folder

--- src/components/LogoNavBarItem.vue
+++ src/components/LogoNavBarItem.vue
@@ -1,11 +1,27 @@
 <template>
   <b-link class="navbar-brand" to="#">
-    <img class="navbar-brand-full" src="img/brand/logo.svg" width="89" height="25" alt="CoreUI Logo">
-    <img class="navbar-brand-minimized" src="img/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo">
+    <img
+      v-for="data in logoNavBarItems"
+      v-bind:key="data.alt_title"
+      v-bind:class="data.class"
+      v-bind:src="data.img_src_url"
+      v-bind:width="data.width"
+      v-bind:height="data.height"
+      v-bind:alt="data.alt_title"
+
+    />
   </b-link>
 </template>
 <script>
+import json from '../configs/logoNavBarItems.json'
+
 export default {
+
   name: 'LogoNavBarItem',
+  data(){
+    return{
+      logoNavBarItems: json
+    }
+  },
 }
 </script>
