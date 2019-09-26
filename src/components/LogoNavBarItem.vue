bb04f1650abd09a100fe316d3fe7b67e33f510e0
Maximiliano Sorribas <mjsorribas@gmail.com>
2019-09-26 15:13:36 -0300

    Update DefaultHeader and add new component LogoNavBarItem

---
+++ src/components/LogoNavBarItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <b-link class="navbar-brand" to="#">
+    <img class="navbar-brand-full" src="img/brand/logo.svg" width="89" height="25" alt="CoreUI Logo">
+    <img class="navbar-brand-minimized" src="img/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo">
+  </b-link>
+</template>
+<script>
+export default {
+  name: 'LogoNavBarItem',
+}
+</script>
