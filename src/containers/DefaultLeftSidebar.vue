5fe7b8c8ac2eebe67f3940c6caa5402435243674
Maximiliano Sorribas <mjsorribas@gmail.com>
2019-10-03 23:04:40 -0300

    add new component DefaultLeftSidebar

---
+++ src/containers/DefaultLeftSidebar.vue
@@ -0,0 +1,31 @@
+<template>
+  <AppSidebar fixed>
+    <SidebarHeader/>
+    <SidebarForm/>
+    <SidebarNav :navItems=navData.items></SidebarNav>
+    <SidebarFooter/>
+    <SidebarMinimizer/>
+  </AppSidebar>
+</template>
+
+<script>
+import nav from "@/_nav"
+import { Sidebar as AppSidebar, SidebarFooter, SidebarForm, SidebarHeader, SidebarMinimizer, SidebarNav } from '@coreui/vue'
+
+export default {
+  name: 'DefaultLeftSidebar',
+  components: {
+    AppSidebar,
+    SidebarForm,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarNav,
+    SidebarMinimizer,
+  },
+  data(){
+    return{
+      navData: nav,
+    }
+  },
+}
+</script>
