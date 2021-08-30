0db3b2181d706cfea63dbe6b84259c45c7273f63
Łukasz Holeczek <lukasz@holeczek.pl>
2021-08-30 22:19:05 +0200

    refactor: use createWebHashHistory instead of createWebHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { h, resolveComponent } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import DefaultLayout from '@/layouts/DefaultLayout'
 
@@ -296,7 +296,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 })
 
