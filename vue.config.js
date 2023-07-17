211531ae7a16f1c367274c7c9ddb964d27c5b27d
mrholek <lukasz@holeczek.pl>
2023-07-17 10:08:15 +0200

    build: update publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/demos/vue/4.2/free/' : '/',
   transpileDependencies: true,
 })
