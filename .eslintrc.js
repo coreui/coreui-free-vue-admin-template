96682c080866a664073a82ff4ac68e5c1260299a
mrholek <lukasz@holeczek.pl>
2023-09-27 12:15:35 +0200

    chore: update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["plugin:vue/vue3-recommended", "eslint:recommended"],
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
