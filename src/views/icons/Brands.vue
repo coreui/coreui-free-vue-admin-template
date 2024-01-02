288559e0cc43a615596bf212a399f104b3b34fe1
mrholek <lukasz@holeczek.pl>
2024-01-02 20:08:42 +0100

    chore: clean-up

--- src/views/icons/Brands.vue
+++ src/views/icons/Brands.vue
@@ -1,11 +1,6 @@
 <template>
   <CRow>
     <CCol>
-      <DocsCallout
-        name="CoreUI Brand Icons"
-        href="components/icon"
-        content="CoreUI Brand Icons. CoreUI Icons package is delivered with more than 1500 icons in multiple formats SVG, PNG, and Webfonts. CoreUI Icons are beautifully crafted symbols for common actions and items. You can use them in your digital products for web or mobile app."
-      />
       <CCard>
         <CCardHeader>CoreUI Icons - Brand</CCardHeader>
         <CCardBody>
